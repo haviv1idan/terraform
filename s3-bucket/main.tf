93b619c759b329abcadfb3d596a9472877ff4781
haviv1idan <haviv1idan@gmail.com>
2025-11-21 14:07:30 +0200

    add random to bucket name

--- s3-bucket/main.tf
+++ s3-bucket/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1"
+    }
   }
 }
 
@@ -12,9 +16,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "random_string" "bucket_suffix" {
+  length  = 8
+  special = true
+  upper   = true
+}
 
 resource "aws_s3_bucket" "dev-bucket" {
-  bucket = "my-tf-dev-bucket"
+  bucket = "my-tf-dev-bucket-${random_string.bucket_suffix.result}"
 
   tags = {
     Name        = "My bucket"
