c1838d8f253221c7b30ebad9f080e08ceeed9835
haviv1idan <haviv1idan@gmail.com>
2025-11-28 16:51:48 +0200

    update s3-bucket, add s3 backend and dynamodb table

--- s3-bucket/main.tf
+++ s3-bucket/main.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket = "tf-state-bucket-for-project"
+    key    = "s3-bucket/terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -16,17 +22,41 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "random_string" "bucket_suffix" {
-  length  = 8
-  special = false
-  upper   = false
+# Create an S3 bucket for Terraform state
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "tf-state-bucket-for-project" 
+  force_destroy = true
 }
 
-resource "aws_s3_bucket" "dev-bucket" {
-  bucket = "my-tf-dev-bucket-${random_string.bucket_suffix.result}"
+# Enable versioning on the S3 bucket
+resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+  # Enable versioning
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable server-side encryption on the S3 bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
+  bucket        = aws_s3_bucket.terraform_state.bucket 
+
+  # Enable server-side encryption
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Create a DynamoDB table for Terraform state locking
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
